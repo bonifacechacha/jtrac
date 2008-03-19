7a467e432bcd1535a792a833597acbf345ce86e0
ptrthomas <ptrthomas@4b96f565-400f-0410-97aa-c8149df7cab3>
2008-03-19 18:10:53 +0000

    tag 2.1.0 release, better late than never

--- jtrac-mylyn/src/info/jtrac/mylyn/exception/HttpException.java
+++
@@ -1,9 +0,0 @@
-package info.jtrac.mylyn.exception;
-
-public class HttpException extends Exception {
-	
-	public HttpException(String message) {
-		super(message);
-	}
-
-}
--- jtrac-mylyn/src/info/jtrac/mylyn/exception/InvalidRefIdException.java
+++
@@ -1,9 +0,0 @@
-package info.jtrac.mylyn.exception;
-
-public class InvalidRefIdException extends Exception {
-
-    public InvalidRefIdException(String message) {
-        super(message);
-    }	
-	
-}
